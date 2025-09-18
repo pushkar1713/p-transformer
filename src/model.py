1851b9321c8a330c7459fa26a7f8a2ced5a3263d
Pushkar Aggarwal <pushkar1713@gmail.com>
2025-09-18 11:35:44 +0000

    done postional encoding

--- src/model.py
+++ src/model.py
@@ -13,3 +13,26 @@ class InputEmbedding(nn.Module):
 
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model : int, seq_length : int, dropout : float):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_length = seq_length
+        self.dropout = nn.Dropout(dropout)
+    
+        pe = torch.zeros(seq_length, d_model)
+
+        position = torch.arange(0, seq_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe.unsqueeze(0) # pe.shape => (1, seq_length, d_model)
+
+        self.register_buffer("pe", pe)
+
+    def forward(self,x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False)
+
